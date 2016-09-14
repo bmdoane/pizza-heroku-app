18e0667863c10e2981f325d0e829866483527325
Brian Doane <brianmdoane@gmail.com>
2016-09-14 11:02:02 -0500

    added order.pug, order.js and getting order to robomongo

--- database.js
+++ database.js
@@ -1,19 +1,11 @@
 'use strict'
 // changes out mongodb for mongoose
+// The databse is available throughout the app
 const mongoose = require('mongoose')
 
 const MONGODB_URL = 'mongodb://localhost:27017/roadkillpizza'
 
 // Says use native node promise library (instead of mongoose promise)
 mongoose.Promise = Promise
-
-// Mongoose model
-// Args - file, fields and datatypes
-mongoose.model('Contact', {
-	name: String,
-	email: String,
-	phone: String,
-	message: String
-})
 
 module.exports.connect = () => mongoose.connect(MONGODB_URL)
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@
 
 const { Router } = require('express')
 const router = Router()
-
-const { db } = require('../database')
+const Contact = require('../models/contact')
+const Order = require('../models/order')
 
 router.get('/', (req, res) =>
   res.render('index')
 )
+
 
 router.get('/about', (req, res) =>
   res.render('about', { page: 'About' })
@@ -17,14 +18,22 @@ router.get('/contact', (req, res) =>
   res.render('contact', { page: 'Contact' })
 )
 
-const mongoose = require('mongoose')
-// Convention to capitalize
-const Contact = mongoose.model('Contact')
 // If you use promises, catch the errors
-
 router.post('/contact', (req, res) => {
 	//console.log(req.body)
 	const msg = new Contact(req.body)
+	msg.save()
+  	.then(() => res.redirect('/'))
+  	.catch(() => res.send('BAD'))
+})
+
+router.get('/order', (req, res) =>
+  res.render('order', {page: 'Order'})
+)
+
+router.post('/order', (req, res) => {
+	//console.log(req.body)
+	const msg = new Order(req.body)
 	msg.save()
   	.then(() => res.redirect('/'))
   	.catch(() => res.send('BAD'))
