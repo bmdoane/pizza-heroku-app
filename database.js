20ff285dcc93a427c9dd24b89ff8af9bb207eb70
Brian Doane <brianmdoane@gmail.com>
2016-09-14 09:58:39 -0500

    added mongoose to replace mongodb

--- database.js
+++ database.js
@@ -1,12 +1,19 @@
 'use strict'
-
-const { MongoClient: { connect } } = require('mongodb')
+// changes out mongodb for mongoose
+const mongoose = require('mongoose')
 
 const MONGODB_URL = 'mongodb://localhost:27017/roadkillpizza'
 
-// Declaring db globally
-let db
+// Says use native node promise library (instead of mongoose promise)
+mongoose.Promise = Promise
 
-module.exports.connect = () => connect(MONGODB_URL).then(_db => db = _db)
-// Getter for a private variable
-module.exports.db = () => db
+// Mongoose model
+// Args - file, fields and datatypes
+mongoose.model('Contact', {
+	name: String,
+	email: String,
+	phone: String,
+	message: String
+})
+
+module.exports.connect = () => mongoose.connect(MONGODB_URL)
--- routes/index.js
+++ routes/index.js
@@ -17,14 +17,17 @@ router.get('/contact', (req, res) =>
   res.render('contact', { page: 'Contact' })
 )
 
+const mongoose = require('mongoose')
+// Convention to capitalize
+const Contact = mongoose.model('Contact')
 // If you use promises, catch the errors
+
 router.post('/contact', (req, res) => {
 	//console.log(req.body)
-  db().collection('contact')
-  	.insertOne(req.body)
+	const msg = new Contact(req.body)
+	msg.save()
   	.then(() => res.redirect('/'))
   	.catch(() => res.send('BAD'))
 })
-
 
 module.exports = router
