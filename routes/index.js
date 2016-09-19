ea05178018395d35c8bb36a24de71a210af577c2
Brian Doane <brianmdoane@gmail.com>
2016-09-19 13:45:48 -0500

    added login and register pugs with index.js routing

--- routes/index.js
+++ routes/index.js
@@ -64,4 +64,28 @@ router.post('/order', (req, res, err) => {
     .catch(err)
 })
 
+router.get('/login', (req, res) =>
+  res.render('login')
+)
+
+router.post('/login', (req, res) => {
+  if(req.body.password === 'password') {
+    res.redirect('/')
+  } else {
+    res.render('login', { error: `Email and password combination do not match`})
+  }
+})
+
+router.get('/register', (req, res) =>
+  res.render('register')
+)
+
+router.post('/register', (req, res) => {
+  if(req.body.password === req.body.confirmation) {
+    res.redirect('/')
+  } else {
+    res.render('register', { error: `Password and password combination do not match`})
+  }
+})
+
 module.exports = router
