3b838d2a0870cd2177bbdb4e4d2dfc4c0fc8b7f2
Brian Doane <brianmdoane@gmail.com>
2016-09-20 09:49:03 -0500

    user login and registration working, reg going to db

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 'use strict'
 
 const { Router } = require('express')
+const bcrypt = require('bcrypt')
+
 const router = Router()
 
 const Contact = require('../models/contact')
 const Order = require('../models/order')
 const Size = require('../models/size')
 const Topping = require('../models/topping')
+const User = require('../models/user')
 
 router.get('/', (req, res) =>
   res.render('index')
@@ -68,23 +71,73 @@ router.get('/login', (req, res) =>
   res.render('login')
 )
 
-router.post('/login', (req, res) => {
-  if(req.body.password === 'password') {
-    res.redirect('/')
-  } else {
-    res.render('login', { error: `Email and password combination do not match`})
-  }
+router.post('/login', ({ body: { email, password } }, res, err) => {
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        return new Promise((resolve, reject) => {
+          bcrypt.compare(password, user.password, (err, matches) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(matches)
+            }
+          })
+        })
+      } else {
+        res.render('login', { msg: 'Email does not exist in our system' })
+      }
+    })
+    .then((matches) => {
+      if (matches) {
+        res.redirect('/')
+      } else {
+        res.render('login', { msg: 'Password does not match' })
+      }
+    })
+    .catch(err)
 })
+
 
 router.get('/register', (req, res) =>
   res.render('register')
 )
 
-router.post('/register', (req, res) => {
-  if(req.body.password === req.body.confirmation) {
-    res.redirect('/')
+// Works - initial post code
+// router.post('/register', (req, res, next) => {
+//   bcrypt.hash(req.body.password, 10, (err, hash) => {
+//     User 
+//       .create({ email: req.body.email, password: hash })
+//       .then(() => res.redirect('/'))
+//       .catch(err => next(err))
+//   })
+// })
+
+// Returns from form
+router.post('/register', ({ body: { email, password, confirmation } }, res, err) => {
+  if (password === confirmation) {
+    User.findOne({ email })
+      .then(user => {
+        if (user) {
+          res.render('register', { msg: 'Email is already registered' })
+        } else {
+          return new Promise((resolve, reject) => {
+            // Fat arrow did not work here
+            bcrypt.hash(password, 13, function(err, hash) {
+              if (err) {
+                reject(err)
+              } else {
+                resolve(hash)
+              }
+            })
+          })
+        }
+      })
+      .then(hash => User.create({ email, password: hash }))
+      .then(() => res.redirect('/login'), { msg: 'User created' })
+      .catch(err)
   } else {
-    res.render('register', { error: `Password and password combination do not match`})
+    res.render('register', { msg: 'Password & password confirmation do not match' })
   }
 })
 
