d40977d65baa49995f77d09679a3e68c8f9b5f64
Brian Doane <brianmdoane@gmail.com>
2016-09-21 11:23:12 -0500

    added logout controller and refactored logout

---
+++ controllers/logout.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports.edit = (req, res) =>
+    res.render('logout', { page: 'Logout'})
+
+
+module.exports.destroy = (req, res) => {
+  req.session.destroy(err => {
+    if (err) throw err
+    res.redirect('/login')
+  })
+}
